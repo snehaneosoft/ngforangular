9fc18fbb93139d571f672c629bbed3bdc4d7eeef
snehaneosoft <snehaupadhyayneosoft@gmail.com>
2022-05-11 13:01:29 +0530

    ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Assignment18_4Demo';
+
+  bookArray = [
+    new Book(1,"abc",20),
+    new Book(2,"abc1",20),
+    new Book(3,"abc123",20)
+  ]
+
+}
+
+class Book{
+  bookId:number;
+  bookName:string;
+  bookPrice:number;
+  constructor(bookId:number, bookName:string, bookPrice:number){
+    this.bookId = bookId;
+    this.bookName = bookName;
+    this.bookPrice = bookPrice; 
+  }
+
+
 }
